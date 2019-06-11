9e150109f88c7e6d245a74b0e12445720dc913ee
Morgan Brown <brown.mhg+git@gmail.com>
2019-06-11 16:51:18 +0200

    Add API endpoint to perform kirbytext-ization

---
+++ site/config/config.php
@@ -0,0 +1,44 @@
+<?php
+return [
+    'debug' => true,
+    'api' => [
+        'basicAuth' => true,
+        'allowInsecure' => true
+    ],
+    'routes' => [
+        [
+            'pattern' => '/rest/(:all)',
+            'method'  => 'GET',
+            'env'     => 'api',
+            'action'  => function ($path = null) {
+                $kirby = kirby();
+                $request = $kirby->request();
+                $render = $kirby->api()->render($path, $this->method(), [
+                    'body'    => $request->body()->toArray(),
+                    'headers' => $request->headers(),
+                    'query'   => $request->query()->toArray(),
+                ]);
+                $decoded = json_decode($render, true);
+                $p = 0;
+                // Kirbytags
+                // Kirbytext-ize any field whose key contains text
+                function ktt($array) {
+                    foreach ($array as $key => $value) {
+                        if (is_array($value)) {
+                            $array[$key] = ktt($value);
+                        } else {
+                          $contains = 'text';
+                          if( strpos( $key, $contains ) !== false ) {
+                            $array[$key] = kirbytext($value);
+                          }
+                        }
+                    }
+                    return $array;
+                }
+                $decoded = ktt($decoded);
+                $encoded = json_encode($decoded, true);
+                return $encoded;
+            }
+        ]
+    ],
+];
